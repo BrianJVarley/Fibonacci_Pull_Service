77b0468103af24484e07a04d1858b33e9cc6c4b7
BrianJVarley <brianvarley9@gmail.com>
2014-12-10 16:44:31 +0000

    Wednesday third commit

--- src/ie/gmit/FibonacciServlet.java
+++ src/ie/gmit/FibonacciServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloServlet extends HttpServlet {
+public class FibonacciServlet extends HttpServlet {
 	
 	
 	private static final long serialVersionUID = 1L;
